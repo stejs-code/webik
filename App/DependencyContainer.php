862f05777db8d50742ccbb9eb19fd8013a426e67
Tom Stejskal <tom@balon.cz>
2025-04-06 21:21:02 +0200

    chore: update composer dependencies and remove Elasticsearch client

--- App/DependencyContainer.php
+++ App/DependencyContainer.php
@@ -2,22 +2,19 @@
 
 namespace App;
 
-use Elastic\Elasticsearch\ClientBuilder;
-use Elastic\Elasticsearch\Client;
 use GuzzleHttp\Psr7\ServerRequest;
 use stdClass;
 
 
 class DependencyContainer
 {
-    public Client $es;
+
     public ServerRequest $request;
     public stdClass $parameters;
     public function __construct()
     {
         try {
             $this->request = ServerRequest::fromGlobals();
-            $this->es = ClientBuilder::create()->build();
             $this->parameters = new stdClass();
         } catch (\Exception $e) {
             die($e->getMessage());
